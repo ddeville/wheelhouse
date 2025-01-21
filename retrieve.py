a7b87b3636160197fd0b97071f1d27b21b645f77
Damien Deville <damien@ddeville.me>
2025-01-20 23:20:43 -0800

    Write to workflows

--- retrieve.py
+++ retrieve.py
@@ -3,6 +3,51 @@ import json
 import sys
 import urllib.error
 import urllib.request
+from pathlib import Path
+from typing import Literal, cast
+
+WORKFLOWS_DIR = Path(__file__).parent / ".github" / "workflows"
+
+PURE_TEMPLATE = """name: $$FULLNAME$$
+on:
+  workflow_dispatch:
+jobs:
+  build_wheels:
+    name: Build wheel
+    runs-on: ubuntu-24.04
+    steps:
+      - uses: actions/checkout@v4
+      - uses: actions/setup-python@v5
+        with:
+          python-version: 3.11
+      - run: ./scripts/build_python_wheel.sh $$SDIST_URL$$
+      - uses: actions/upload-artifact@v4
+        with:
+          name: wheel
+          path: ./wheelhouse/*.whl
+"""
+
+NATIVE_TEMPLATE = """name: $$FULLNAME$$
+on:
+  workflow_dispatch:
+jobs:
+  build_wheels:
+    name: Build wheel on ${{ matrix.os }}
+    runs-on: ${{ matrix.os }}
+    strategy:
+      matrix:
+        os: [ubuntu-24.04, ubuntu-24.04-arm, macos-13, macos-14]
+    steps:
+      - uses: actions/checkout@v4
+      - uses: actions/setup-python@v5
+        with:
+          python-version: 3.11
+      - run: ./scripts/build_native_wheel.sh $$SDIST_URL$$
+      - uses: actions/upload-artifact@v4
+        with:
+          name: wheel-${{ matrix.os }}
+          path: ./wheelhouse/*.whl
+"""
 
 
 def get_sdist_url(package: str, version: str) -> str | None:
@@ -23,22 +68,44 @@ def get_sdist_url(package: str, version: str) -> str | None:
     return None
 
 
+def ask_package_type() -> Literal["pure", "native"]:
+    while True:
+        resp = (
+            input(
+                "Is the package pure Python or does it contain native code? [pure, native]: "
+            )
+            .strip()
+            .lower()
+        )
+        if resp in ["pure", "native"]:
+            return cast(Literal["pure", "native"], resp)
+
+
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--name", required=True)
     parser.add_argument("-v", "--version")
     args = parser.parse_args()
 
+    fullname = f"{args.name}=={args.version}"
     url = get_sdist_url(args.name, args.version)
 
     if url is None:
         print(
-            f"Couldn't find an sdist for {args.name}=={args.version} on pypi.org",
+            f"Couldn't find an sdist for {fullname} on pypi.org",
             file=sys.stderr,
         )
         sys.exit(1)
 
-    print(url)
+    match ask_package_type():
+        case "pure":
+            template = PURE_TEMPLATE
+        case "native":
+            template = NATIVE_TEMPLATE
+
+    contents = template.replace("$$FULLNAME$$", fullname).replace("$$URL$$", url)
+    with open(WORKFLOWS_DIR / f"{fullname}.yaml", "w") as f:
+        f.write(contents)
 
 
 if __name__ == "__main__":
